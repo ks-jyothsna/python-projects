a5b2dcc9727d775ac5b193573934b227b225cc11
ks-jyothsna <jyothsnaks03@gmail.com>
2025-10-31 21:51:12 -0400

    Update Maze.py

--- 06_Reeborg-the-robot/Maze.py
+++ 06_Reeborg-the-robot/Maze.py
@@ -3,6 +3,11 @@ def turn_right():
     turn_left()
     turn_left()
 
+#Workaround to solve exceptions of infinite loop, Solution: make sure there is a wall in front, and turn left to start.
+while front_is_clear():
+    move()
+turn_left()
+
 while not at_goal():
     if right_is_clear():
         turn_right()
